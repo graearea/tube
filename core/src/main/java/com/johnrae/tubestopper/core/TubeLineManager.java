4700685c01e8e830909edc455603395ccb236d99
[1;32m[KUser Name:[m[K John Rae (jrae) <jrae@yazino.com>
2014-08-28 00:37:34 +0100

    first running test in core...

--- core/src/main/java/com/johnrae/tubestopper/core/TubeLineManager.java
+++ core/src/main/java/com/johnrae/tubestopper/core/TubeLineManager.java
@@ -2,25 +2,51 @@ package com.johnrae.tubestopper.core;
 
 import java.awt.*;
 import java.io.File;
-import java.util.HashSet;
-import java.util.Set;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.List;
 
-/**
- * Created by jrae on 24/06/2014.
- */
 public class TubeLineManager {
-    public TubeLineManager() {
-        final String file = getClass().getResource("lines.csv").getFile();
-        final boolean exists = new File(file).exists();
-        System.out.println(exists);
-//        final File file = new File("/Users/jrae/blah.txt");
-//        final boolean exists = file.exists();
 
+    private final List<String> lines;
+
+    public TubeLineManager() {
+        try {
+            lines = Files.readAllLines(Paths.get(getClass().getResource("lines.csv").toURI()));
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
     }
     public Set<TubeLine> getTubeLines(){
         final HashSet<TubeLine> tubeLines = new HashSet<TubeLine>();
-        tubeLines.add(new TubeLine(1, "northern", Color.BLACK));
+        final String[] headers = lines.get(0).split(",");
+        verifyHeaders(headers);
+        lines.remove(0);
+        for (String line : lines) {
+
+            final String[] field = line.split(",");
+            tubeLines.add(new TubeLine(Integer.valueOf(field[0]),field[1],Color.getColor(field[2])));
+        }
         return tubeLines;
+
+    }
+
+    private void verifyHeaders(final String[] headers) {
+        String[] expectedHeaders = new String[]{"line","name","colour","stripe"};
+        for (int i = 0; i < expectedHeaders.length; i++) {
+            if(!expectedHeaders[i].equals(headers[i])) {
+                System.out.println(headers);
+                throw new RuntimeException(expectedHeaders[i]+" header not present:");
+            }
+
+        }
 
     }
 }
--- core/src/test/java/com/johnrae/tubestop/app/TubeLineManagerTest.java
+++ core/src/test/java/com/johnrae/tubestop/app/TubeLineManagerTest.java
@@ -2,8 +2,6 @@ package com.johnrae.tubestop.app;
 
 import com.johnrae.tubestopper.core.TubeLine;
 import com.johnrae.tubestopper.core.TubeLineManager;
-import org.hamcrest.CoreMatchers;
-import org.hamcrest.core.IsEqual;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -11,6 +9,11 @@ import org.junit.Test;
 import java.awt.*;
 import java.util.Set;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.hasItem;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -30,15 +33,15 @@ public class TubeLineManagerTest {
     @Test
     public void getTubeLinesShouldReturnASetContainingAtLeastOneLine() {
         final Set<TubeLine> tubeLines = underTest.getTubeLines();
-        assertTrue(tubeLines.size() > 0);
+        assertThat(tubeLines.size(),equalTo(13));
     }
 
     @Test
-    public void Should() {
+    public void dataShouldContainBakerlooLine() {
         final Set<TubeLine> tubeLines = underTest.getTubeLines();
         final Color brown = Color.getColor("ab6612");
-        assertTrue(tubeLines.contains(new TubeLine(1,"Bakerloo Line", brown)));
-
+        assertThat(new TubeLine(1, "Bakerloo Line", brown), equalTo(new TubeLine(1, "Bakerloo Line", brown)));
+        assertThat(tubeLines, hasItem(new TubeLine(1, "Bakerloo Line", brown)));
 
     }
 }
